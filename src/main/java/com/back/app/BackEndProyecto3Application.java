b0c51e30bcc55bef22470e3085e3b677c0fda604
Sergio <sergio.cescit@gmail.com>
2021-03-17 09:54:50 +0100

    hola

---
+++ src/main/java/com/back/app/BackEndProyecto3Application.java
@@ -0,0 +1,13 @@
+package com.back.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndProyecto3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndProyecto3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/app/BackEndProyecto3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndProyecto3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
