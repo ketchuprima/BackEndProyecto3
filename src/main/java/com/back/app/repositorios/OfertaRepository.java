a4cf49094330e1557606ba4a78edcfca5fbff953
Sergio <sergio.cescit@gmail.com>
2021-03-24 12:30:56 +0100

    XD

--- src/main/java/com/back/app/repositorios/OfertaRepository.java
+++ src/main/java/com/back/app/repositorios/OfertaRepository.java
@@ -11,7 +11,7 @@ import com.back.app.modelos.Oferta;
 @Repository
 public interface OfertaRepository extends JpaRepository<Oferta, Long>, OfertaRepositoryCustom{
 	
-	@Query(value = "SELECT * FROM oferta WHERE estat = :estat WHERE data_de_publicacio > DATE_SUB(now(), INTERVAL 3 MONTH) ORDER BY data_de_publicacio DESC", nativeQuery = true)
+	@Query(value = "SELECT * FROM oferta WHERE estat = :estat AND data_de_publicacio > DATE_SUB(now(), INTERVAL 3 MONTH) ORDER BY data_de_publicacio DESC", nativeQuery = true)
 	public List<Oferta> findByEstat(Integer estat);
 
 	public List<Oferta> findfertasByParameters(Long categoriaObj, String ubicacio, String titol, Long empresa,
